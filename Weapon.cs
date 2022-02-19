64a46273315e0e5bc5b528700aca1cbd24ccf29f
Ktveh <lemuriyskiy@rambler.ru>
2022-02-19 08:54:20 +0300

    Create Weapon.cs

---
+++ Weapon.cs
@@ -0,0 +1,37 @@
+﻿class Weapon
+{
+    private int _damage;
+    private int _bullets;
+
+    public void Fire(Player player)
+    {
+        if (_bullets > 0)
+        {
+            _bullets -= 1;
+            player.TakeDamage(_damage);
+        }
+    }
+}
+
+class Player
+{
+    private int _health;
+
+    public void TakeDamage(int damage)
+    {
+        if (_damage > 0)
+            _health -= damage;
+        if (_health < 0)
+            _health = 0;
+    }
+}
+
+class Bot
+{
+    private Weapon _weapon;
+
+    public void OnSeePlayer(Player player)
+    {
+        _weapon.Fire(player);
+    }
+}
