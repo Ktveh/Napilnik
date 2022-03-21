a79ab7388cd6cc0792148450073805f62f38e992
Ktveh <lemuriyskiy@rambler.ru>
2022-03-21 10:15:52 +0300

    Change 01

--- Weapon.cs
+++ Weapon.cs
@@ -1,34 +1,58 @@
-﻿class Weapon
+﻿public class Weapon
 {
     private int _damage;
-    private int _bullets;
+    private int _countBullets;
+
+    public Weapon(int damage, int countBullets)
+    {
+        if (damage < 0)
+            throw new ArgumentOutOfRangeException();
+        _damage = damage;
+
+        if (countBullets < 0)
+            throw new ArgumentOutOfRangeException();
+        _countBullets = countBullets;
+    }
 
     public void Fire(Player player)
     {
-        if (_bullets > 0)
+        if ((!player.IsDead) && (_countBullets > 0))
         {
-            _bullets -= 1;
+            _countBullets--;
             player.TakeDamage(_damage);
         }
     }
 }
 
-class Player
+public class Player
 {
     private int _health;
 
+    public bool IsDead => _health <= 0;
+
+    public Player(int health)
+    {
+        if (health <= 0)
+            throw new ArgumentOutOfRangeException();
+        _health = health;
+    }
+
     public void TakeDamage(int damage)
     {
-        if (_damage > 0)
-            _health -= damage;
+        _health -= damage;
         if (_health < 0)
             _health = 0;
     }
 }
 
-class Bot
+public class Bot
 {
     private Weapon _weapon;
+
+    public Bot(Weapon weapon)
+    {
+        _weapon = weapon;
+    }
 
     public void OnSeePlayer(Player player)
     {
